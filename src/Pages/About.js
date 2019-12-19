7eab384ba5e3969002297d84a58d20b4c27e16a1
ernimg <mglernest@gmail.com>
2019-12-19 21:11:46 +0100

    create  social icons  and interests, experience section.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FaUser,FaFacebookF,FaLinkedinIn} from 'react-icons/fa'
+import {FaUser,FaFacebookF,FaLinkedinIn,FaMusic,FaFilm,FaSwimmer,FaBook, FaAngleRight} from 'react-icons/fa';
+import '../Style/About.css';
 const About = () =>{
 
     return(
@@ -7,12 +8,13 @@ const About = () =>{
         <div className="about-wrapper">
             <div className="about__header">
                 <div className="about__header-icon">
-                    <FaUser/>
+                    <FaUser className="about__header-icon-item"/>
                 </div>
+                <h1 className="about__header-username">Imie Nazwisko</h1>
             </div>
             <div className="about__body">
                 <div className="about__body-paragraf">
-                    <p>
+                    <p className="about__body-paragraf-p">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad totam ipsum facere deserunt asperiores excepturi voluptas officiis ullam esse aliquam, earum autem rem, eligendi maiores cumque cum laudantium et architecto.
                     </p>
                 </div>
@@ -21,11 +23,42 @@ const About = () =>{
                         <a href="#"><FaFacebookF/></a>
                         <a href="#"><FaLinkedinIn/></a>
                     </div>
-                    <div className="experience">
-                        Nie wiem co dać
+                    <div className="interests">
+                       <h2 className="interests-header" >
+                           Zainteresowania
+                       </h2>
+                       <div className="interests-icosns">
+                           <div className="interests-icosns__items">
+                            <FaMusic className="music"/><span>Muzyka</span>
+                           </div>
+                           <div lassName="interests-icosns__items">
+                            <FaFilm className="film"/><span>Film</span>
+                           </div>
+                           <div lassName="interests-icosns__items">
+                            <FaSwimmer  className="swiming"/><span>Pływanie</span>
+                           </div>
+                           <div lassName="interests-icosns__items">
+                            <FaBook className="book"/><span>Dobra książka</span>
+                           </div>
+                       </div>
                     </div>
                     <div className="skils">
-                        nie wiem co dać
+                        <h2 className="skils-header" >
+                            Doświadczenie
+                        </h2>
+                        <div className="skils-box">
+                            <div>
+                                <div lassName="">
+                                    <FaAngleRight className="arr"/><span>Kurs pierszej pomocy przedmedycznej</span>
+                                </div>  
+                            </div>
+                            <div lassName="">
+                                <FaAngleRight className="arr"/><span>Studia podyplomowe w zakresie przygotowania pedagogicznego</span> 
+                            </div>
+                            <div lassName="">
+                                <FaAngleRight className="arr"/><span> Kwalifikacyjny kurs pedagogiczny dla nauczycieli praktycznej nauki zawodu </span> 
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
