abcbdd661527373315a6696e05cc71456c71a70e
ernimg <mglernest@gmail.com>
2020-01-09 15:10:13 +0100

    error page

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../Style/ErrPage.css';
 
 const ErrorPage = () =>{
 
     return(
         <div>
-            ErrorPage
+            
+        <h1 class="sentence"><span>4</span><span>0</span><span>4</span><span> </span><span>P</span><span>a</span><span>g</span><span>e</span><span> </span><span>
+            N</span><span>o</span><span></span><span> </span><span>F</span><span>o</span><span>u</span><span>n</span><span>d</span>
+        </h1>
         </div>
     )
 }
